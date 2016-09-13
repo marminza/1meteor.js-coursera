964fcab19b7dd30db7eec15178d1324ef5a93637
marminza <marmnes@etsii.upv.es>
2016-09-14 00:04:44 +0200

    week1 finished

--- week1/images_meteor/client/main.js
+++ week1/images_meteor/client/main.js
@@ -1,22 +1,23 @@
 import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+// Template.images.onCreated(function imagesOnCreated() {
+//   // counter starts at 0
+//   this.counter = new ReactiveVar(0);
+// });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+// Template.images.events({
+//   'click button'(event, instance) {
+//     // increment the counter when button is clicked
+//     instance.time.set(instance.time.get() + 1);
+//   },
+// });
+
+
+	var today = new Date();
+
+	Template.times.helpers({time:today});
+ 
